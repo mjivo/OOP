fdc5d9ea7f1e1bd7f5f40a780466f45132e3179f
Ivo Petrov <mjivo175@abv.bg>
2022-07-20 12:19:13 +0300

    Exceptions and Error Handling - Lab

---
+++ Exceptions and Error Handling - Lab/Square Root/Num.cs
@@ -0,0 +1,25 @@
+﻿namespace Square_Root
+{
+    using System;
+    public class Num
+    {
+        private int _value;
+        public Num(int value)
+        {
+            this.Value = value;
+        }
+        public int Value
+        {
+            get => this._value;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidCastException("Invalid number.");
+                }
+                this._value = value;
+            }
+        }
+        public double SquareRoot => Math.Sqrt(this.Value);
+    }
+}
---
+++ Exceptions and Error Handling - Lab/Square Root/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Square_Root
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
